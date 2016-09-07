3b2ddf4d46a9bb07b592791f60776b0bda5bdaf8
andrey <casumarzu@yandex.ru>
2016-09-07 16:59:29 +0300

    Add cross-env

--- src/scripts/containers/Test.js
+++ src/scripts/containers/Test.js
@@ -1,14 +1,11 @@
 import React, { Component, PropTypes } from 'react'
 import { Router, Route, Link, browserHistory } from 'react-router'
-import mui from 'material-ui'
 
-export default class App extends Component {
-  componentWillMount() {}
-
+export default class Test extends Component {
   render() {
     return (
       <div>
-        <h1>Hello!</h1>
+        <h1>Test!</h1>
       </div>
     )
   }
